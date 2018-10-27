8572d50c9041bb0cfec0aa883901cea7afa17e3b
QaysTrad <qaystrad@gamil.com>
2018-10-27 14:05:59 +0200

    add ui packeges

---
+++ react-client/build/precache-manifest.50ffc15bf12b063a4261bba111b912d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8bfaeb9ec1744f423c9c",
+    "url": "/static/js/main.8bfaeb9e.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "8bfaeb9ec1744f423c9c",
+    "url": "/static/css/main.b8b29804.chunk.css"
+  },
+  {
+    "revision": "261cbe4e2a0a5ba874d7e8a7f032a963",
+    "url": "/index.html"
+  }
+];
