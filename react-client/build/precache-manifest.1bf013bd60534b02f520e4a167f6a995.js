38b476c2a941587ff412a6cc0740ef0995d0cfd1
QaysTrad <qaystrad@gamil.com>
2018-10-27 16:03:21 +0200

    build folder

---
+++ react-client/build/precache-manifest.1bf013bd60534b02f520e4a167f6a995.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4eb9a43e640f2baf25ab",
+    "url": "/static/js/main.4eb9a43e.chunk.js"
+  },
+  {
+    "revision": "c9d39ec9da665c398ee8",
+    "url": "/static/js/1.c9d39ec9.chunk.js"
+  },
+  {
+    "revision": "4eb9a43e640f2baf25ab",
+    "url": "/static/css/main.b9b387e2.chunk.css"
+  },
+  {
+    "revision": "80cb34ae6f230b38c7dc9d4b0cb9daf0",
+    "url": "/index.html"
+  }
+];
