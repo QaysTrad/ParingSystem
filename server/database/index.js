b86951999f2fd79ce76036fdcbf5d9f0576f25be
QaysTrad <qaystrad@gamil.com>
2018-10-28 13:22:39 +0200

    update database to mlab

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://student:Jackel12@ds135433.mlab.com:35433/students');
+mongoose.connect('mongodb://qays:Qays123@ds255539.mlab.com:55539/paringsys');
 mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 
